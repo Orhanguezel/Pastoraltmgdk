62ed5bca8b6de610587e93f5d41bf890298f5a34
Orhan Güzel <orhan.guezel@dci-student.org>
2024-12-23 02:29:57 +0100

    env dotev

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const cors = require("cors"); // CORS için eklendi
+require("dotenv").config(); // .env dosyasını yükler
 
 const app = express();
 const PORT = 3005;
